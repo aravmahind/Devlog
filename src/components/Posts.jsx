1b455ddf0347c88ebdddec7cec2f16d9b51a2876
Your Name <your.email@example.com>
2025-05-31 16:27:11 +0530

    Add code Highlighter for the post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function AddPost({t, d, p}) {
     return (
         <>
         <div>Title - {t}</div>
         <div>Description - {d}</div>
-        <pre className="bg-gray-300">
-            <code>{p}</code>
-        </pre>
+        <SyntaxHighlighter language="cpp" style={atomDark}>
+            {p}
+        </SyntaxHighlighter>
         </>
     )
 }
@@ -33,16 +35,16 @@ function Posts() {
 
     return (
         <>
-        <div className="m-10">
-            <input type="text" className="w-full" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter the Title..." required></input><br></br>
-            <input type="text" className="w-full" value={desc} onChange={(e) => setDesc(e.target.value)} placeholder="Enter the Description..."></input><br></br>
-            <textarea rows={8} className="w-full" value={post} onChange={(e) => setPost(e.target.value)} placeholder="Post here..." required></textarea><br></br>
-            <button className="border-2" disabled={!title || !post} onClick={handlePost}>Post</button>
+        <div className="m-23">
+            <input type="text" className="w-full border-1" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter the Title..." required></input><br></br>
+            <input type="text" className="w-full border-1" value={desc} onChange={(e) => setDesc(e.target.value)} placeholder="Enter the Description..."></input><br></br>
+            <textarea rows={8} className="w-full border-1" value={post} onChange={(e) => setPost(e.target.value)} placeholder="Post here..." required></textarea><br></br>
+            <button className="border-1 p-3 bg-emerald-700 text-white" disabled={!title || !post} onClick={handlePost}>Post</button>
         </div>
 
         {posts.length > 0 && ( 
             posts.map((p, i) => (
-            <div key={i} className="m-10">
+            <div key={i} className="m-23">
                 <div>Post - {i+1}</div>
                     <AddPost t={p.title} d={p.desc} p={p.post}/>
                 <br></br>
